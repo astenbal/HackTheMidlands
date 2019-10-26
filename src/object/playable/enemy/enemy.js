c4c52d796d86745e20c2721153e75928168f7007
Craig Leek <cqhd.leek@gmail.com>
2019-10-26 22:54:16 +0100

    Move collision from randomenemy to enemy

--- src/object/playable/enemy/enemy.js
+++ src/object/playable/enemy/enemy.js
@@ -54,6 +54,27 @@ class Enemy extends PlayableObject {
             this.shootCounter = 0;
         }
         this.shootCounter++;
+        if(this.position[0] < this.moveRectangle[0][0])
+            this.position[0] = this.moveRectangle[0][0]
+        if(this.position[0] > this.moveRectangle[0][1])
+            this.position[0] = this.moveRectangle[0][1]
+        if(this.position[1] < this.moveRectangle[1][0])
+            this.position[1] = this.moveRectangle[1][0]
+        if(this.position[1] > this.moveRectangle[1][1])
+            this.position[1] = this.moveRectangle[1][1]
+        this.hitbox.x = this.position[0];
+        this.hitbox.y= this.position[1];
+        for(const object of game.objects){
+            if(object == this)
+                continue;
+            if(this.overlap(object)){
+                if(this.position[0] != this.oldX)
+                    this.position[0] = this.oldX;
+                if(this.position[1] != this.oldY)
+                    this.position[1] = this.oldY;
+                break;
+            }
+        }
     }
 
     draw(canvas) {
