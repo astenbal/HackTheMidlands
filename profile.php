4cca61f69e024e934045e54fcf44d0ad162b4970
Jay Marriner <jaymarriner123@gmail.com>
2019-10-27 03:29:51 +0000

    Profile stuff

--- profile.php
+++ profile.php
@@ -5,7 +5,9 @@
         <title>Hackatank</title>
         <!--Link to the css file-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel = "stylesheet" type = "text/css" href = "style/highscore.css" />
         <link rel = "stylesheet" type = "text/css" href = "style/main.css" />
+        <link rel = "stylesheet" type = "text/css" href = "style/profile.css" />
     </head>
 
     <body>
@@ -13,6 +15,38 @@
       <?php include 'includes/nav.php';?>
 
       <main class='mainArea'>
+        <section id='proName'><?php echo $_SESSION['username']; ?></section>
+        <section id='medalStorage'>
+          <img src='images/medal.png'>
+        </section>
+        <section id='userHighs'>
+          <table>
+            <tr id='tblTitles'>
+              <th>Pos</th>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+
+            <?php
+            $user = $_SESSION['username'];
+            $sql = "SELECT * FROM score WHERE name = '$user' ORDER BY score DESC";
+            $res = $conn->query($sql);
+            $counter = 1;
+            while($row=$res->fetch_assoc()){
+              $name = $row['name'];
+              $score = $row['score'];
+              echo'
+              <tr class="score">
+                <td> #'.$counter.'</td>
+                <td>'.$name.'</td>
+                <td>'.$score.'</td>
+              </tr>
+              ';
+              $counter++;
+            }
+            ?>
+          </table>
+        </section>
       </main>
       <?php include ("includes/footer.php"); ?>
     </body>
